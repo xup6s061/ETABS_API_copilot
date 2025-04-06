4d400adc72b4c6a09a549652801fba8d22218cb0
Nan <Nan@MSI>
2025-04-06 21:18:33 +0800

    加入專案檔案。

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Runtime.Remoting.Messaging;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
+using System.Windows;
+using ETABSv1; // 確保你已經引用了 ETABS API 的命名空間
+
+namespace YourNamespace
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ ViewModels/MainViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Windows;
+using ETABSv1; // 確保你已經引用了 ETABS API 的命名空間
+using System.Windows.Input;
+
+namespace YourNamespace.ViewModels
+{
+    public class MainViewModel : BaseViewModel
+    {
+        public ICommand OpenETABSCommand { get; }
+
+        public MainViewModel()
+        {
+            OpenETABSCommand = new RelayCommand(OpenETABS);
+        }
+
+        private void OpenETABS()
+        {
+            try
+            {
+                // 創建 ETABS 應用程序對象
+                cOAPI etabsObject = null;
+                cHelper myHelper = new Helper();
+                etabsObject = myHelper.CreateObjectProgID("CSI.ETABS.API.ETABSObject");
+
+                // 打開 ETABS
+                etabsObject.ApplicationStart();
+                MessageBox.Show("ETABS 已成功打開！");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("打開 ETABS 時發生錯誤: " + ex.Message);
+            }
+        }
+    }
+}
